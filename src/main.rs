4fc644dea917de257f9a7b8519dcbb1e4959da76
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-06 10:00:51 +1100

    Add project structure
    Added some structure to the project, including a system to allow running a specific day or part, and a file reading utility

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod util;
+mod days;
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        eprintln!("Usage: {} <day> <part>", args[0]);
+        std::process::exit(1);
+    }
+
+    let day: u32 = args[1].parse()?;
+    let part: u32 = args[2].parse()?;
+
+    let input = util::read_input(day);
+
+    match (day, part) {
+        (1, 1) => println!("{}", days::day01::part1(&input)),
+        _ => anyhow::bail!("Day {day} part {part} is not implemented"),
+    }
+
+    Ok(())
 }
---
+++ src/util.rs
@@ -0,0 +1,8 @@
+use std::fs;
+
+pub fn read_input(day: u32) -> String {
+    let file = format!("inputs/day{:02}.txt", day);
+    fs::read_to_string(&file).unwrap_or_else(|_| {
+        panic!("Failed to read input file: {}", file);
+    })
+}
