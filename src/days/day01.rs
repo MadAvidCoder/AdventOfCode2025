ce47602cb6404ef22708da5c99f124c39d90d64b
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-06 10:35:04 +1100

    Day 1 - Part 1
    Added a solution for day 1, part 1 of AOC

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,5 +1,28 @@
 pub fn part1(input: &str) -> i32 {
-    0
+    let mut loc: i32 = 50;
+    let mut code: i32 = 0;
+    for line in input.lines() {
+        let dir: char = line.chars().next().unwrap();
+        let amt: i32 = line[dir.len_utf8()..].parse().unwrap();
+        match dir {
+            'L' => loc -= amt,
+            'R' => loc += amt,
+            _ => panic!("{} is invalid", dir),
+        }
+
+        if loc < 0 {
+            loc += 100;
+        } else if loc >= 100 {
+            loc -= 100;
+        }
+
+        loc = loc % 100;
+
+        if loc == 0 {
+            code += 1;
+        }
+    }
+    code
 }
 
 pub fn part2(input: &str) -> i32 {
