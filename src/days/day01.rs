8f1a8da943d71fbdbb00400ccb4837178f7647a0
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-06 11:25:30 +1100

    Day 1 - Part 2
    Added my solution for part 2 of day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -26,5 +26,29 @@ pub fn part1(input: &str) -> i32 {
 }
 
 pub fn part2(input: &str) -> i32 {
-    0
+    let mut loc: i32 = 50;
+    let mut code: i32 = 0;
+    for line in input.lines() {
+        let dir: char = line.chars().next().unwrap();
+        let amt: i32 = line[dir.len_utf8()..].parse().unwrap();
+        let delta: i32 = match dir {
+            'L' => -amt,
+            'R' => amt,
+            _ => panic!("{} is invalid", dir),
+        };
+
+        let pos: i32 = loc.rem_euclid(100);
+
+        let mut dist_to_zero = if delta > 0 { (100 - pos) % 100 } else { pos % 100 };
+        if dist_to_zero == 0 {
+            dist_to_zero = 100;
+        }
+
+        if amt >= dist_to_zero {
+            code += 1 + (amt - dist_to_zero) / 100;
+        }
+
+        loc = (loc + delta).rem_euclid(100);
+    }
+    code
 }
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn main() -> Result<()> {
 
     match (day, part) {
         (1, 1) => println!("{}", days::day01::part1(&input)),
+        (1, 2) => println!("{}", days::day01::part2(&input)),
         _ => anyhow::bail!("Day {day} part {part} is not implemented"),
     }
 
