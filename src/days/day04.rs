744447531de05c1a819b759ce68b6d1548225d2d
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-09 17:42:28 +1100

    Day 4 - Part 2
    Added the second part of day 4

--- src/days/day04.rs
+++ src/days/day04.rs
@@ -49,5 +49,58 @@ pub fn part1(input: &str) -> u32 {
 }
 
 pub fn part2(input: &str) -> u32 {
-    0
+    let mut acc: u32 = 0;
+
+    let mut grid: [[u8; 135]; 135] = [[0; 135]; 135];
+    for (r, line) in input.lines().enumerate() {
+        for (c, ch) in line.chars().enumerate() {
+            if ch == '@' {
+                grid[r][c] = 1;
+            }
+        }
+    }
+
+    let dirs: [(isize, isize); 8] = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),            (0, 1),
+        (1, -1),   (1, 0),  (1, 1),
+    ];
+
+    loop {
+        let mut cur_it: u32 = 0;
+        for r in 0..135 {
+            for c in 0..135 {
+                if grid[r][c] == 0 {
+                    continue;
+                }
+
+                let mut count: u8 = 0;
+
+                for (dr, dc) in dirs {
+                    let nr = r as isize + dr;
+                    let nc = c as isize + dc;
+
+                    if nr < 0 || nr >= 135 || nc < 0 || nc >= 135 {
+                        continue;
+                    }
+
+                    count += grid[nr as usize][nc as usize];
+
+                    if count >= 4 {
+                        break;
+                    }
+                }
+
+                if count < 4 {
+                    cur_it += 1;
+                    grid[r][c] = 0;
+                }
+            }
+        }
+        if cur_it == 0 {
+            break;
+        }
+        acc += cur_it;
+    }
+    acc
 }
