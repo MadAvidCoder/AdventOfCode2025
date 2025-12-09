8d48c77ed931dafc6b21e2076c8b2bc41e5cf1fb
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-09 18:11:00 +1100

    Day 5 - Part 2
    Added my solution for day 5 part 2

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -27,6 +27,36 @@ pub fn part1(input: &str) -> u32 {
     count
 }
 
-pub fn part2(input: &str) -> u32 {
-    0
+pub fn part2(input: &str) -> u64 {
+    let mut count: u64 = 0;
+
+    let mut fresh: Vec<(u64, u64)> = input
+        .split("\r\n\r\n")
+        .collect::<Vec<&str>>()[0]
+        .lines()
+        .map(|s| {
+            let mut nums = s.split('-');
+            let start = nums.next().unwrap().parse::<u64>().unwrap();
+            let end = nums.next().unwrap().parse::<u64>().unwrap();
+            (start, end)
+        })
+        .collect::<Vec<(u64,u64)>>();
+    fresh.sort_by_key(|k| k.0);
+
+    let mut merged: Vec<(u64, u64)> = Vec::new();
+    for (start, end) in fresh {
+        if let Some(last) = merged.last_mut() {
+            if start <= last.1 + 1 {
+                last.1 = last.1.max(end);
+                continue;
+            }
+        }
+        merged.push((start, end));
+    }
+
+    for (start, end) in merged {
+        count += (end - start + 1);
+    }
+
+    count
 }
