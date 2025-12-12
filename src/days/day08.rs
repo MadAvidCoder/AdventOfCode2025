f37e23fb97ccace793b8ef862c4726c5576a172a
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-12 20:02:23 +1100

    Day 8 - Part 2
    Added my solution for part 2 of day 8

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -5,13 +5,15 @@ use std::cmp::Reverse;
 struct DSU {
     rep: Vec<usize>,
     size: Vec<usize>,
+    components: usize,
 }
 
 impl DSU {
     fn new(n: usize) -> Self {
         DSU {
             rep: (0..n).collect(),
-            size: vec![1; n]
+            size: vec![1; n],
+            components: n,
         }
     }
 
@@ -34,6 +36,7 @@ impl DSU {
 
         self.rep[y_root] = x_root;
         self.size[x_root] += self.size[y_root];
+        self.components -= 1;
     }
 
     fn get_sizes(&mut self) -> Vec<usize> {
@@ -96,6 +99,65 @@ pub fn part1(input: &str) -> u32 {
     sizes.iter().take(3).product::<usize>() as u32
 }
 
-pub fn part2(input: &str) -> u32 {
-    0
+pub fn part2(input: &str) -> u64 {
+    let points: Vec<[f64;3]> = input
+        .lines()
+        .map(|line| {
+            let coords = line
+                .split(',')
+                .map(|num| num
+                    .trim()
+                    .parse::<f64>()
+                    .unwrap()
+                )
+                .collect::<Vec<f64>>();
+            [coords[0], coords[1], coords[2]]
+        })
+        .collect();
+
+
+    let mut tree: KdTree<f64, 3> = KdTree::new();
+
+    for (i, point) in points.iter().enumerate() {
+        tree.add(point, i as u64);
+    }
+
+    let mut neighbours: Vec<Vec<(i64, usize)>> = Vec::with_capacity(points.len());
+    let mut cursor: Vec<usize> = vec![1usize; points.len()];
+    let mut heap: BinaryHeap<Reverse<(i64, usize, usize)>> = BinaryHeap::new();
+
+    for point in points.iter() {
+        neighbours.push(tree
+            .nearest_n::<SquaredEuclidean>(point, points.len())
+            .into_iter()
+            .map(|n| (n.distance as i64, n.item as usize))
+            .collect::<Vec<(i64, usize)>>()
+        );
+    }
+
+    for i in 0..points.len() {
+        let (dist, b) = neighbours[i][1];
+        heap.push(Reverse((dist, i, b)));
+    }
+
+    let mut dsu: DSU = DSU::new(points.len());
+    let mut last_edge: (usize, usize) = (0, 0);
+
+    while dsu.components > 1 {
+        let Reverse((dist, a, b)) = heap.pop().unwrap();
+
+        if dsu.find(a) != dsu.find(b) {
+            dsu.union(a, b);
+            last_edge = (a, b);
+        }
+
+        cursor[a] += 1;
+
+        if cursor[a] < neighbours[a].len() {
+            let (dist, b) = neighbours[a][cursor[a]];
+            heap.push(Reverse((dist, a, b)));
+        }
+    }
+
+    points[last_edge.0][0] as u64 * points[last_edge.1][0] as u64
 }
