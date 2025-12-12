e2d4e5e3f562cd25e1a264f2c24cbf39c479cb22
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2025-12-12 18:43:10 +1100

    Day 8 - Part 1
    Solved part 1 of the day 8 problem

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,5 +1,99 @@
+use kiddo::{KdTree, SquaredEuclidean};
+use std::collections::BinaryHeap;
+use std::cmp::Reverse;
+
+struct DSU {
+    rep: Vec<usize>,
+    size: Vec<usize>,
+}
+
+impl DSU {
+    fn new(n: usize) -> Self {
+        DSU {
+            rep: (0..n).collect(),
+            size: vec![1; n]
+        }
+    }
+
+    fn find(&mut self, k: usize) -> usize {
+        if self.rep[k] != k {
+            self.rep[k] = self.find(self.rep[k]);
+        }
+        self.rep[k]
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let mut x_root = self.find(x);
+        let mut y_root = self.find(y);
+
+        if x_root == y_root { return; }
+
+        if self.size[x_root] < self.size[y_root] {
+            std::mem::swap(&mut x_root, &mut y_root);
+        }
+
+        self.rep[y_root] = x_root;
+        self.size[x_root] += self.size[y_root];
+    }
+
+    fn get_sizes(&mut self) -> Vec<usize> {
+        let mut sizes = vec![0; self.rep.len()];
+        for i in 0..self.rep.len() {
+            sizes[self.find(i)] += 1;
+        }
+        sizes.into_iter().filter(|&c| c > 0).collect()
+    }
+}
+
 pub fn part1(input: &str) -> u32 {
-    0
+    let points: Vec<[f64;3]> = input
+        .lines()
+        .map(|line| {
+            let coords = line
+                .split(',')
+                .map(|num| num
+                    .trim()
+                    .parse::<f64>()
+                    .unwrap()
+                )
+                .collect::<Vec<f64>>();
+            [coords[0], coords[1], coords[2]]
+        })
+        .collect();
+
+
+    let mut tree: KdTree<f64, 3> = KdTree::new();
+
+    for (i, point) in points.iter().enumerate() {
+        tree.add(point, i as u64);
+    }
+
+    let mut edges = Vec::new();
+
+    for (i, point) in points.iter().enumerate() {
+        let neighbours = tree.nearest_n::<SquaredEuclidean>(point, points.len());
+
+        for n in neighbours {
+            if n.item as usize > i {
+                edges.push((n.distance as i64, i, n.item as usize));
+            }
+        }
+    }
+
+    edges.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
+
+    let shortest = &edges[..edges.len().min(1000)];
+
+    let mut dsu = DSU::new(points.len());
+
+    for &(_, a, b) in shortest {
+        dsu.union(a, b);
+    }
+
+    let mut sizes = dsu.get_sizes();
+    sizes.sort_unstable_by(|a, b| b.cmp(a));
+
+    sizes.iter().take(3).product::<usize>() as u32
 }
 
 pub fn part2(input: &str) -> u32 {
